070f739fa0db708008794f3bdc5740467678f23a
thisari hettiarachchi <msthisari@gmail.com>
2025-07-05 13:29:45 +0530

    new

--- components/connect.php
+++ components/connect.php
@@ -8,4 +8,16 @@
     } catch (PDOException $e) {
         die("Connection failed: " . $e->getMessage());
     }
+
+    if (!function_exists('unique_id')) {
+        function unique_id() {
+            $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charLength = strlen($chars);
+            $randomString = '';
+            for ($i = 0; $i < 20; $i++) {
+                $randomString .= $chars[mt_rand(0, $charLength - 1)];
+            }
+            return $randomString;
+        }
+    }
 ?>
---
+++ register.php
@@ -0,0 +1,55 @@
+<?php
+    
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Register</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+        <link rel="stylesheet" href="css/user_styles.css">
+    </head>
+
+    <body>
+        <section class="form-ontainer">
+            <form action="" method="post" enctype="multipart/form-data" class="register">
+                <h3>Register Now</h3>
+                <div class="flex">
+                    <div class="col">
+                        <div class="input-field">
+                            <p>Your Name <span>*</span></p>
+                            <input type="text" name="name" placeholder="Enter your name" maxlength="50" required class="box">
+                        </div>
+                        <div class="input-field">
+                            <p>Your Email <span>*</span></p>
+                            <input type="email" name="email" placeholder="Enter your email" maxlength="50" required class="box">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="input-field">
+                            <p>Your Password <span>*</span></p>
+                            <input type="password" name="pass" placeholder="Enter your password" maxlength="50" required class="box">
+                        </div>
+                        <div class="input-field">
+                            <p>Your Password <span>*</span></p>
+                            <input type="password" name="cpass" placeholder="Confirm your password" maxlength="50" required class="box">
+                        </div>
+                    </div>
+                </div>
+                    <p class="link">Already have an account? <a href="login.php">Login</a></p>
+                    <input type="submit" name="submit" value="register now" class="btn">
+            </form>
+        </section>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert">
+
+        <?php include 'components/alert.php'; ?>
+    </body>
+
+</html>
