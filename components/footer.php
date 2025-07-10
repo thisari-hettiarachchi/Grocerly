544b4fecf776fb5942a396982de08929a0ddc2d8
Thisari Hettiarachchi <msthisari@gmail.com>
2025-07-10 14:12:06 +0530

    footer

--- components/footer.php
+++ components/footer.php
@@ -85,3 +85,47 @@
 </div>
 
 
+
+
+<footer>
+    <div class="content">
+        <div class="box">
+            <h3>My Account</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>My Account</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Order History</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Wishlist</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Newsletter</a>
+        </div>
+        <div class="box">
+            <h3>Information</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>About Us</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Deliver Information</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Privacy Policy</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Terms & Conditions</a>
+        </div>
+        <div class="box">
+            <h3>Extras</h3>
+            <a href=""><i class="bx bx-chevron-right"></i>Brands</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Gift Certification</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Affiliate</a>
+            <a href=""><i class="bx bx-chevron-right"></i>Specials</a>
+        </div>
+        <div class="box">
+            <h3>Contact Us</h3>
+            <p><i class="bx bxs-phone"></i>0756984236</p>
+            <p><i class="bx bxs-envelope"></i>grocerly@gmail.com</p>
+            <p><i class="bx bxs-location-plus"></i>Colombo, Sri Lanka</p>
+            <div class="icons">
+                <i class="bx bxl-facebook"></i>
+                <i class="bx bxl-twitter"></i>
+                <i class="bx bxl-instagram"></i>
+                <i class="bx bxl-linkedin"></i>
+            </div>
+        </div>
+    </div>
+
+    <div class="bottom">
+        <p>&copy; 2025 Grocerly | All Rights Reserved</p>
+        <a href="admin panel/login.php">Become A Seller</a>
+    </div>
+</footer>
