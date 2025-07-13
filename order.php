11a1afce84939864137e278b1181b24743dbca3e
chanchala <jeewanthichanchala321@gmail.com>
2025-07-13 15:36:53 +0530

    order page

---
+++ order.php
@@ -0,0 +1,47 @@
+<?php 
+    include 'components/connect.php';
+
+    if (isset($_COOKIE['user_id'])) {
+        $user_id = $_COOKIE['user_id'];
+    }else{
+        $user_id = '';
+    }
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Order Page</title>
+
+        <link rel="stylesheet" type="text/css" href="css/user.css">
+        
+        <link rel="shortcut icon" href="images/fav.png" type="image/svg+xml">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+        <link href="https://unpkg.com/boxicons@2.1/css/boxicons.min.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <?php include('components/user_header.php'); ?>
+
+        
+
+        <script src="js/user_script.js"></script>
+        <?php include('components/alert.php'); ?>
+        <?php include('components/footer.php'); ?>
+
+    </body>
+
+</html>
+
+
+
